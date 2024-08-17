52b8a448bf1c5ea49f3225a08e0af3868b19ff03
vishakh <vishakh@farefirst.com>
2024-08-17 11:47:47 +0530

    "initial commit"

--- Git_pratical/ViewController.swift
+++ Git_pratical/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello world")
+        print("I'm going to checkout the last version.")
     }
 
 
