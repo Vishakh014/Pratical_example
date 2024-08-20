555a6060f6361b8520d5bbb82a2bec77efde48d1
vishakh <vishakh@farefirst.com>
2024-08-20 10:54:16 +0530

    final commmit

--- Api.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Api.swift
-//  Git_pratical
-//
-//  Created by Vishakh on 17/08/24.
-//
-
-import Foundation
-var api_key = "akjsbgaklbfgklsagsfnaksdbfgksnggfkoangt"
---
+++ Git_pratical/api.swift
@@ -0,0 +1,9 @@
+//
+//  Api.swift
+//  Git_pratical
+//
+//  Created by Vishakh on 17/08/24.
+//
+
+import Foundation
+var api_key = "akjsbgaklbfgklsagsfnaksdbfgksnggfkoangt"
